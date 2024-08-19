dfb75ebde86b8000eb4cd7f211f0255577239dff
Levi Lucena <sr_lucen@yahoo.com.br>
2024-08-19 13:08:24 -0300

    Add files via upload

--- usuarios/inserir_usuario.php
+++ usuarios/inserir_usuario.php
@@ -23,7 +23,7 @@ function conectarBanco()
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "gestão";
+    $dbname = "gestao";
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Erro na conexão com o banco de dados: " . $conn->connect_error);
